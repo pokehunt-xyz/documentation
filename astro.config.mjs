b5aafce02e9dce606c00255e82cdc1c5794198df
Robin-Sch <46354497+Robin-Sch@users.noreply.github.com>
2025-04-27 10:35:58 +0200

    Added a few examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'PokéHunt',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/pokehunt-xyz' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'How to start', slug: 'guides/start' },
 					],
 				},
 				{
